3efb18e4c740c7ce482c5d30c7738c1c3cc7fa97
Gwartney <davidg@boundary.com>
2015-09-24 18:03:30 -0700

    change counter

--- get-counters.ps1
+++ get-counters.ps1
@@ -1,6 +1,7 @@
 $metric_id = 'PHYSICAL_DISK_QUEUE_LENGTH'
 $source = Get-Content Env:\COMPUTERNAME
 $counter_name = '\physicaldisk(_total)\current disk queue length'
+$counter_name = '\Processor(_total)\% User Time'
 
 while($true)
 {
