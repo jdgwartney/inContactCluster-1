07d45df9f26d32d0ff160cced82c0610f202d16c
Gwartney <davidg@boundary.com>
2015-09-24 17:56:43 -0700

    Incorrect variable for host

--- get-counters.ps1
+++ get-counters.ps1
@@ -7,7 +7,7 @@ while($true)
     $counter_value =  (get-counter -counter $counter_name).countersamples | select -expandproperty cookedvalue
     $current_time = get-date -UFormat %s
     $timestamp = [math]::round($current_time)
-    write-host $metric_id $counter_value $hostname $timestamp
+    write-host $metric_id $counter_value $source $timestamp
     [Console]::Out.Flush()
 }
 
