d80253860551d860efa5414b0f200dc80ada9f34
Gwartney <davidg@boundary.com>
2015-09-24 17:51:09 -0700

    simple timestamp

--- get-counters.ps1
+++ get-counters.ps1
@@ -5,8 +5,7 @@ $counter_name = '\physicaldisk(_total)\current disk queue length'
 while($true)
 {
     $counter_value =  (get-counter -counter $counter_name).countersamples | select -expandproperty cookedvalue
-    $date = (get-date -UFormat %s)
-    $timestamp = [math]::round($date)
+    $timestamp = (get-date -UFormat %s)
     write-host $metric_id $counter_value $hostname $timestamp
     [Console]::Out.Flush()
 }
